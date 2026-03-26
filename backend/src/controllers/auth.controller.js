1f33aeb09249b71d0060d063617188f880108ee1
Rishabh <ry257541@gmail.com>
2026-03-26 23:48:27 +0530

    update profile

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -3,6 +3,7 @@ import bcrypt from 'bcryptjs'
 import { generateToken } from '../lib/utils.js'
 import { sendWelcomeEmail } from '../emails/emailhandlers.js'
 import { ENV } from '../lib/env.js'
+import cloudinary from '../lib/cloudinary.js'
 
 export const signup = async (req, res) => {
     const { fullName, email, password } = req.body
@@ -65,7 +66,7 @@ export const signup = async (req, res) => {
 export const login = async(req, res)=>{
     const {email, password} = req.body;
 
-    if(!email || password)
+    if(!email || !password)
         res.status(400).json({message:"Email and Password are required"})
     try{
         const user = await User.findOne({email});
@@ -95,4 +96,25 @@ export const login = async(req, res)=>{
 export const logout = (req, res)=>{
     res.cookie("jwt","", {maxAge:0})
     res.status(200).json({message:"Logged Out Successfully"})
+}
+
+export const updateProfile = async(req,res) => {
+    try {
+        const {profilePic} = req.body;
+        if(!profilePic)
+        return res.status(400).json({message:"Profile pic is required"})
+        
+        const userId = req.user._id
+
+        const uploadResponse = await cloudinary.uploader.upload(profilePic)
+
+        const updatedUser = await User.findByIdAndUpdate(userId, {profilePic: uploadResponse.secure_url}, {new: true});
+        
+        res.status(200).json(updatedUser)
+
+    } catch (error) {
+        console.log("Error in update profile: ",error)
+        res.status(500).json({message:"Internal Server Error"})
+
+    }
 }
---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,27 @@
+import jwt, { decode } from "jsonwebtoken"
+import User from '../models/User.js'
+import { ENV } from '../lib/env.js'
+
+export const protectRoute = async (req, res, next)=>{
+    try {
+        const token  = req.cookies.jwt;
+        if(!token)
+            return res.status(401).json({message:"Unauthorized - No token provided"})
+
+        const decoded = jwt.verify(token,ENV.JWT_SECRET)
+        if(!decoded)
+            return res.status(401).json({message:"Unauthorized - Invalid Token"})
+
+        const user = await User.findById(decoded.userId).select("-password") //select everthing but not the password
+        if(!user)
+            return res.status(404).json({message:"User not found"})
+
+        //if every checks above this are successfull then the user is able to update his profile otheriwse not , next() help to move towards the next route to which requires these check 
+        req.user = user
+        next()
+
+    } catch (error) {
+        console.log("Error in protectRoute midddleware ", error);
+        return res.status(500).json({message:"Internal Server Error"})
+    }
+}
