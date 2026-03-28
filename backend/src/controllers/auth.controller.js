a54e9d8e530afd61c59e0746b3ad581b1dc00ee8
Rishabh <ry257541@gmail.com>
2026-03-29 01:27:21 +0530

    added sidebar functionality

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -98,23 +98,24 @@ export const logout = (req, res)=>{
     res.status(200).json({message:"Logged Out Successfully"})
 }
 
-export const updateProfile = async(req,res) => {
-    try {
-        const {profilePic} = req.body;
-        if(!profilePic)
-        return res.status(400).json({message:"Profile pic is required"})
-        
-        const userId = req.user._id
+export const updateProfile = async (req, res) => {
+  try {
+    const { profilePic } = req.body;
+    if (!profilePic) return res.status(400).json({ message: "Profile pic is required" });
 
-        const uploadResponse = await cloudinary.uploader.upload(profilePic)
+    const userId = req.user._id;
 
-        const updatedUser = await User.findByIdAndUpdate(userId, {profilePic: uploadResponse.secure_url}, {new: true});
-        
-        res.status(200).json(updatedUser)
+    const uploadResponse = await cloudinary.uploader.upload(profilePic);
 
-    } catch (error) {
-        console.log("Error in update profile: ",error)
-        res.status(500).json({message:"Internal Server Error"})
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { profilePic: uploadResponse.secure_url },
+      { new: true }
+    );
 
-    }
-}
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    console.log("Error in update profile:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,10 +1,10 @@
 import express from "express"
 import { signup, login, logout, updateProfile } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
-// import { arcjetProtection } from "../middleware/arcjet.middleware.js";   //for checking whether bots or spam or illegal requsts
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";   //for checking whether bots or spam or illegal requsts
 
 const router = express.Router();
-// router.use(arcjetProtection);
+router.use(arcjetProtection);
 
 router.post('/signup', signup)
 router.post('/login', login)
